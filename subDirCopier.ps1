fbba02fe12f32ba12d41c063632e0c6fc61a7d9e
binarybuddha <github@binarybuddha.com>
2016-04-28 13:59:15 -0600

    Update subDirCopier.ps1

--- subDirCopier.ps1
+++ subDirCopier.ps1
@@ -1,3 +1,7 @@
 $src = "c:\sauce"
 $dst = "c:\place"
+dir $src -Recurse
+dir $dst -Recurse
 dir $src -Directory | select -ExcludeProperty FullName | Copy-Item -Destination $dst -Recurse
+dir $src -Recurse
+dir $dst -Recurse
